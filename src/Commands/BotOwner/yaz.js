aadd0b3441af2de6b9dd0a58fa2903521a25b42d
Kvalue <103127564+KvalueJS@users.noreply.github.com>
2022-06-16 10:32:22 +0300

    Update yaz.js

--- src/Commands/BotOwner/yaz.js
+++ src/Commands/BotOwner/yaz.js
@@ -5,6 +5,6 @@ module.exports = {
     cooldown: 5000
   }, help: { developer: true, owner: false},
   run: async (client, message, params, embed) => {
-    message.channels.send(message.content)
+    message.channej.send(message.content)
   }
 }
